4f34050429079dc322b4a5363c4fc47a72158bcd
unknown <you@example.com>
2024-07-17 17:06:07 +0600

    Hospital-Management-backend

---
+++ app.js
@@ -0,0 +1,58 @@
+import express from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import { dbConnection } from "./database/dbConnection.js";
+import messageRouter from "./router/messageRouter.js";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+import userRouter from "./router/userRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js";
+
+const app = express();
+config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URI, process.env.DASHBOARD_URI],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+// root
+app.get("/", (req, res) => {
+  res.send("Hey Sinamika");
+});
+
+// application routes using
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
+
+//db connection
+dbConnection();
+
+//error middle using
+app.use(errorMiddleware);
+
+// Handle not found route
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Not Found",
+  });
+});
+
+export default app;
